41b8cd37dd025a402710e0004d5d80f3623310ea
Parpth <38754025+Parpth@users.noreply.github.com>
2020-06-09 19:50:58 +0700

    2D Unity game project
    6088066_6088097_6088172

---
+++ asset/Rsc_Mr.Gun/Assets/Scripts/EnemyBehaviorA1.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviorA1 : MonoBehaviour
+{
+
+    public UnitData unitData;
+    // We need rigidBody2D to make a movement 
+    public Rigidbody2D rigidbody2d;
+    // We need Collider2D to allow collision detection 
+    public Collider2D collider2d;
+
+    public GameObject target;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        if (!rigidbody2d)
+        {
+            rigidbody2d = GetComponent<Rigidbody2D>();
+        }
+        if (!collider2d)
+        {
+            collider2d = GetComponent<Collider2D>();
+        }
+
+        SetupUnitData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!target)
+        {
+            DetectEnemy();
+        }
+        else
+        {
+            FollowTarget();
+        }
+
+    }
+
+    public void SetupUnitData()
+    { // Reference Playground HealthSystemAttribute component 
+        HealthSystemAttribute healthSystem = GetComponent<HealthSystemAttribute>();
+        if (healthSystem)
+        {
+            healthSystem.maxHealth = (int)unitData.maxHealth;
+            healthSystem.health = (int) unitData.health;
+        }
+        ModifyHealthAttribute modifyHealthAttrb = GetComponent<ModifyHealthAttribute>();
+        if (modifyHealthAttrb)
+        {
+            modifyHealthAttrb.healthChange = (int)-unitData.collisionDamage;
+        }
+
+    }
+
+    public void DetectEnemy()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FollowTarget()
+    { // Follow Target 
+        transform.position = Vector2.MoveTowards( 
+            transform.position, 
+            target.transform.position, 
+            unitData.speed * Time.deltaTime);
+    }
+
+    }
---
+++ asset/Rsc_Mr.Gun/Assets/Scripts/Movement/CompanionFollow.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionFollow : MonoBehaviour
+{
+    public float stoppingdistance;
+
+    public UnitData unitData;
+    // We need rigidBody2D to make a movement 
+    public Rigidbody2D rigidbody2d;
+    // We need Collider2D to allow collision detection 
+    public Collider2D collider2d;
+
+    public GameObject target;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        if (!rigidbody2d)
+        {
+            rigidbody2d = GetComponent<Rigidbody2D>();
+        }
+        if (!collider2d)
+        {
+            collider2d = GetComponent<Collider2D>();
+        }
+
+        SetupUnitData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!target)
+        {
+            DetectEnemy();
+        }
+        else
+        {
+            rb = this.GetComponent<Rigidbody2D>();
+            FollowTarget();
+        }
+
+    }
+
+    public void SetupUnitData()
+    { // Reference Playground HealthSystemAttribute component 
+        HealthSystemAttribute healthSystem = GetComponent<HealthSystemAttribute>();
+        if (healthSystem)
+        {
+            healthSystem.maxHealth = (int)unitData.maxHealth;
+            healthSystem.health = (int)unitData.health;
+        }
+        ModifyHealthAttribute modifyHealthAttrb = GetComponent<ModifyHealthAttribute>();
+        if (modifyHealthAttrb)
+        {
+            modifyHealthAttrb.healthChange = (int)-unitData.collisionDamage;
+        }
+
+    }
+
+    public void DetectEnemy()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    void FollowTarget()
+    { // Follow Target 
+        if (Vector2.Distance(transform.position, target.transform.position) > 200)
+        {
+            Vector3 direction = target.transform.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, unitData.speed * Time.deltaTime);
+        }
+    }
+
+}
